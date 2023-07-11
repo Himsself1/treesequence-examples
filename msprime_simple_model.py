d9e71aab1dbd7a0b520a037992822957fc9fc610
Himsself1 <spapadadonakis@gmail.com>
2023-07-11 22:22:06 +0300

    1st commit

---
+++ msprime_simple_model.py
@@ -0,0 +1,14 @@
+import sys
+import math
+from pathlib import Path
+import msprime
+import numpy
+import tskit
+import random
+import time
+# import matplotlib.pyplot as plt
+import gc
+import threading
+
+
+
